6dee7c5c9593649d5c65eedff16dbad19cd5762b
user <user@--Feng-->
2022-11-02 16:15:40 +0800

    first commit

---
+++ src/main/java/com/example/demo_gourmetmap/DemoGourmetmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_gourmetmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGourmetmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGourmetmapApplication.class, args);
+	}
+
+}
